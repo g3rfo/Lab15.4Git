8a44c5394ce5a7be96e3330b8711080418321243
g3rfo <rozhchenkoivan@gmail.com>
2023-12-12 08:41:56 +0200

    Add project files.

---
+++ Lab15.4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <stdio.h>
+#define size 10
+
+int main()
+{
+	int i, j, result = 0;
+
+	int array[size];
+
+	for (i = 0; i < size; i++) {
+		printf("Enter %d number: ", i + 1);
+		scanf_s("%d", &array[i]);
+	}
+
+	for (j = 0; j < size; j++) {
+		result += array[j];
+	}
+
+	printf("Result: %d", result);
+
+	return 0;
+}
